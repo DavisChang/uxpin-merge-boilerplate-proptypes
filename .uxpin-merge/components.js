2b83ef91eead545fa853a20dd74023ea65a0e257
davis_chang <davis chang>
2018-12-10 14:08:31 +0800

    dropdown menu

---
+++ .uxpin-merge/components.js
@@ -0,0 +1,30 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import Button from '../src/Button/Button';
+import Icon from '../src/Icon/Icon';
+import Table from '../src/Table/Table';
+import Select from '../src/Select/Select/Select';
+import SelectItem from '../src/Select/SelectItem/SelectItem';
+import AreaChart from '../src/AreaChart/AreaChart';
+import BarChart from '../src/BarChart/BarChart';
+import LineChart from '../src/LineChart/LineChart';
+import LineMarkChart from '../src/LineMarkChart/LineMarkChart';
+import MarkChart from '../src/MarkChart/MarkChart';
+import PieChart from '../src/PieChart/PieChart';
+import Wrapper from '../src/UXPinWrapper/UXPinWrapper.js';
+export {
+  Button,
+  Icon,
+  Table,
+  Select,
+  SelectItem,
+  AreaChart,
+  BarChart,
+  LineChart,
+  LineMarkChart,
+  MarkChart,
+  PieChart,
+  Wrapper,
+  React,
+  ReactDOM,
+};
